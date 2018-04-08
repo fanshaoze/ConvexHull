18c2602ab11ca7f966baa43adb0905ff0ad906dd
fanshaoze <fanshaoze123123@163.com>
2018-04-08 23:37:31 +0800

    枚举法 start

--- ConvexHull/convex.c
+++ ConvexHull/convex.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 #include "convex.h"
+
+
+//node nodes[10];
+void enumeration(node *nodes) {
+	int i, j, k, g;
+	int isin;
+	int length = sizeof(nodes) / sizeof(nodes[0]);
+	for (i = 0; i < length; i++) {
+		for (j = 0; j < length; j++) {
+			for (k = 0; k < length; k++) {
+				for (g = 0; g < length; g++) {
+					isin = judgein(nodes[i], nodes[j], nodes[k], nodes[g]);
+				}
+			}
+		}
+	}
+}
+int judgein(node a, node b, node c, node d) {
+	return 0;
+}
 int main() {
+	int i = 10;
+	node *nodes;
+	nodes = (node*)malloc(i * sizeof(node));
 	printf("what");
 	return 0;
 
--- ConvexHull/convex.h
+++ ConvexHull/convex.h
@@ -1,3 +1,11 @@
 #pragma once
 #include <stdio.h>
 
+typedef struct 
+{
+	int x;
+	int y;
+}node;
+
+void enumeration(node *nodes);
+int judgein(node a, node b, node c, node d);
